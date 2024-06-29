0e3df7679bedaf18f84475049f5d3405c39aff9e
pamalasomchith <somchithpamala@gmail.com>
2024-06-29 14:13:31 -0700

    commit 2

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ def main():
     ##################################################
     # Comlete your code here
     ##################################################
-    number = int(input('Enter one integer value: '))
+    result = int(input('Enter one integer value: '))
 
-    if number < 0 :
-        print(f'The value {number} is an odd number')
+    if result < 0:
+        print(f'The value {result} is an odd number')
     
-    else number > 0 :
-        print(f'The value {number} is an even number')
+    else:
+        print(f'The value {result} is an even number')
 
 
     ########################################
