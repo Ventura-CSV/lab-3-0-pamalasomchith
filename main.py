c7f06f0fe4d3e9b3052e4a6a9facb54be547999d
pamalasomchith <somchithpamala@gmail.com>
2024-06-29 15:03:03 -0700

    commit 8

--- main.py
+++ main.py
@@ -1,22 +1,13 @@
 def main():
-    ##################################################
-    # Comlete your code here
-    ##################################################
     number = int(input('Enter one number: '))
     if (number % 2) == 0:
         result = 0
-    
     else:
-        result = 1
-    
-         
+        result = 1      
     if result:
         print(f'The result is {result}')
-    
     else:
         print(f'The result is {result}')
-
-
     ########################################
     # Do not delete the return statement
     ########################################
